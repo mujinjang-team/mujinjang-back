600ad1fda2b606277cc364fd7038ed393caa81f2
Jongwoo Han <jongwooo.han@gmail.com>
2023-12-15 17:29:17 +0900

    chore: 정적 분석을 위해 SonarCloud를 도입한다 (#5)
    * chore: SonarCloud 설정을 추가한다
    
    * style: 큰 따옴표를 작은 따옴표로 변경한다
    
    * chore: build.yml을 생성한다
    
    * test: 불필요한 테스트 코드를 제거한다

--- src/test/java/mujinjang/couponsystem/CouponSystemApplicationTests.java
+++ src/test/java/mujinjang/couponsystem/CouponSystemApplicationTests.java
@@ -1,13 +1,8 @@
 package mujinjang.couponsystem;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CouponSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
